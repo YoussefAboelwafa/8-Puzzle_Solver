7dc3def8574400e884b37e910956b9274f2c071c
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-10-20 04:22:40 +0300

    bfs

--- bfs.py
+++ bfs.py
@@ -4,16 +4,16 @@ from utils import *
 
 
 def bfs_solver(start_state, goal_state):
-    fronteir = queue.Queue()
+    frontier = queue.Queue()
     explored = set()
     fronteir_explored = set()
     parent = dict()
-    fronteir.put(start_state)
+    frontier.put(start_state)
     parent[start_state] = start_state
     nodes_expanded = 0
 
-    while not fronteir.empty():
-        curr = fronteir.get()
+    while not frontier.empty():
+        curr = frontier.get()
         explored.add(curr)
         fronteir_explored.add(curr)
         nodes_expanded += 1
@@ -24,18 +24,18 @@ def bfs_solver(start_state, goal_state):
         neighbours = getNeighbours(curr)
         for neighbour in neighbours:
             if neighbour not in fronteir_explored:
-                fronteir.put(neighbour)
+                frontier.put(neighbour)
                 fronteir_explored.add(neighbour)
                 parent[neighbour] = curr
 
     return (False, "")
 
 
-start_time = time.time()          
+start_time = time.time()
 _, res, nodes_expanded = bfs_solver("867254301", "012345678")
 end_time = time.time()
 cost = res[0]
 path = res[1]
 print("Cost:", cost)
 print("Nodes expanded:", nodes_expanded)
-print("Time taken:", (end_time - start_time)*1000, "ms")
+print("Time taken:", (end_time - start_time) * 1000, "ms")
